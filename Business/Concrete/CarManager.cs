487d6a93b0edfd5cc67b221cd5b11ae7967d60b6
Mert Başkaya <baskaya_1999@hotmail.com>
2021-03-28 21:12:16 +0300

    With AOP

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -17,16 +19,10 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if(car.Description.Length >= 2 && car.DailyPrice > 0)
-            {
-                _carDal.Add(car);
-            
-                return new SuccessfulResult(Messages.CarAdded);
-            }
-            return new ErrorResult(Messages.CarInvalid);
-            
+            return new SuccessfulResult(Messages.CarAdded);
         }
 
         public IResult Delete(Car car)
