bca0fcafa18fed315f83bd8195d3666358e5ea60
Mert Başkaya <baskaya_1999@hotmail.com>
2021-03-31 02:04:03 +0300

    Each car has max 5 images (GUID)

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -8,14 +8,17 @@ namespace Business.Constants
 {
     public static class Messages
     {
-        public static string CarAdded = "The car is successfully added!";
-        public static string CarDeleted = "The car is successfully deleted!";
-        public static string CarUpdated = "The car is successfully updated!";
-        public static string CarUpdateInvalid = "The car cannot be updated!";
-        public static string CarInvalid = "The car could not be added!";
-        public static string MaintenanceTime = "System is under maintenance!";
-        public static string CarListed = "Cars are listed!";
-        public static string CarRented = "The car has been rented!";
-        public static string CarRentedInvalid = "The car has been rented already!";
+        public static string CarAdded = "The car is successfully added";
+        public static string CarDeleted = "The car is successfully deleted";
+        public static string CarUpdated = "The car is successfully updated";
+        public static string CarUpdateInvalid = "The car cannot be updated";
+        public static string CarInvalid = "The car could not be added";
+        public static string MaintenanceTime = "System is under maintenance";
+        public static string CarListed = "Cars are listed";
+        public static string CarRented = "The car has been rented";
+        public static string CarRentedInvalid = "The car has been rented already";
+        public static string ImageLimitExceeded = "The car cannot have more than five images";
+
+
     }
 }
