f968559399f4610bf6bc813f28eda730c5dea449
Mert Başkaya <baskaya_1999@hotmail.com>
2021-04-09 22:11:06 +0300

    20

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -94,6 +94,8 @@ namespace WebAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
 
+            app.ConfigureCustomExceptionMiddleware();
+
             app.UseCors(builder => builder.WithOrigins("http://localhost:49205").AllowAnyHeader());
 
             app.UseHttpsRedirection();
